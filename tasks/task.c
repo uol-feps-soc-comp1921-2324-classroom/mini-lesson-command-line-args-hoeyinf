ededa9b4b5ce957e3626c5917662682d776aa3c4
Hoe Yin Foong <146224208+hoeyinf@users.noreply.github.com>
2024-02-04 14:55:24 +0000

    finished task

--- tasks/task.c
+++ tasks/task.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[])
+{
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    // checks that only 3 arguments were entered (1 command + 2 ints)
+    if (argc != 3)
+    {
+        // outputs correct format
+        printf("Usage: %s <num1> <num2>\n", argv[0]);
+        return 0;
+    }
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // checks if the two arguments are valid ints, outputs relevant info if not
+    // specifically checks if it's an invalid string or a float
+    int invalid = 0;
+    if ((atoi(argv[1]) == 0 && *argv[1] != '0') || atoi(argv[1]) != atof(argv[1]))
+    {
+        printf("<num1> must be an integer. '%s' is not valid\n", argv[1]);
+        invalid = 1;
+    }
+    if ((atoi(argv[2]) == 0 && *argv[2] != '0') || atoi(argv[2]) != atof(argv[2]))
+    {
+        printf("<num2> must be an integer. '%s' is not valid\n", argv[2]);
+        invalid = 1;
+    }
 
-    // Calculate and print the sum
-    int sum = num1 + num2;
+    // ends program if there was an invalid input
+    if (invalid == 1)
+    {
+        return 0;
+    }
+
+    // calculates and prints the sum
+    int sum = atoi(argv[1]) + atoi(argv[2]);
     printf("Sum: %d\n", sum);
 
-    return 0; // Exit successfully
+    return 0;
 }
